94fa8fe5ecf981840d3c3a6532f15f0f7bfaf5ea
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2023-06-04 15:59:41 +0530

    Updated UI

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useState, useEffect } from "react";
-
+import Link from "next/link";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className='mt-16 prompt_layout'>
+    <div className='mt-16 prompt_layoutss'>
       {data.map((post) => (
         <PromptCard
           key={post._id}
@@ -67,28 +67,143 @@ const Feed = () => {
     setSearchedResults(searchResult);
   };
 
+  //  to check no. of items
+
+const numPrompts = allPosts.length;
+const uniqueTags = [...new Set(allPosts.map((post) => post.tag))];
+const numTags = uniqueTags.length;
+const uniqueCreators = [...new Set(allPosts.map((post) => post.creator._id))];
+const numCreators = uniqueCreators.length;
+
   return (
-    <section className='feed'>
-      <form className='relative w-full flex-center'>
+    <section className="feedss ">
+      {/* no of post bar */}
+      <div className="w-full justify-center flex items-center">
+        <ul className="list-reset flex">
+          <li className="text-center py-3 px-4 border-b-2 border-solid border-transparent border-teal">
+            <Link
+              href="#"
+              className="text-grey-darker no-underline hover:no-underline"
+            >
+              <div className="text-sm font-bold tracking-tight mb-1">
+                Prompts
+              </div>
+              <div className="text-lg tracking-tight font-bold text-teal">
+                {numPrompts}
+              </div>
+            </Link>
+          </li>
+          <li className="text-center py-3 px-4 border-b-2 border-solid border-transparent hover:border-teal">
+            <Link
+              href="#"
+              className="text-grey-darker no-underline hover:no-underline"
+            >
+              <div className="text-sm font-bold tracking-tight mb-1">Tags</div>
+              <div className="text-lg tracking-tight font-bold hover:text-teal">
+                {numTags}
+              </div>
+            </Link>
+          </li>
+          <li className="text-center py-3 px-4 border-b-2 border-solid border-transparent hover:border-teal">
+            <Link
+              href="#"
+              className="text-grey-darker no-underline hover:no-underline"
+            >
+              <div className="text-sm font-bold tracking-tight mb-1">Users</div>
+              <div className="text-lg tracking-tight font-bold hover:text-teal">
+                {numCreators}
+              </div>
+            </Link>
+          </li>
+         
+        </ul>
+      </div>
+      {/* no of post bar */}
+
+      <form className="flex justify-center items-center">
         <input
-          type='text'
-          placeholder='Search for a tag or a username'
+          type="text"
+          placeholder="Search for Link tag or Link username"
           value={searchText}
           onChange={handleSearchChange}
           required
-          className='search_input peer'
+          className="search_input peer w-full "
         />
       </form>
 
-      {/* All Prompts */}
-      {searchText ? (
-        <PromptCardList
-          data={searchedResults}
-          handleTagClick={handleTagClick}
-        />
-      ) : (
-        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
-      )}
+      {/* feed */}
+
+      <div>
+        <div className="container mx-auto flex flex-col lg:flex-row mt-3 text-sm leading-normal">
+          <div className="w-full  bg-white mb-4">
+            <div className="p-3 text-lg font-bold border-b border-solid border-grey-light">
+              <Link
+                href="#"
+                className="text-black mr-6 no-underline hover-underline"
+              >
+                Posts
+              </Link>
+             
+            </div>
+            <div className="md:w-5/6 -mt-10">
+             
+              {/* All Prompts */}
+              {searchText ? (
+                <PromptCardList
+                  data={searchedResults}
+                  handleTagClick={handleTagClick}
+                />
+              ) : (
+                <PromptCardList
+                  data={allPosts}
+                  handleTagClick={handleTagClick}
+                />
+              )}
+            </div>
+          </div>
+          <div className="w-full lg:w-1/4 pl-4 ">
+            <div className="bg-white p-3 mb-3 ">
+              <div className="mb-3 pb-4 border-b-2 border-solid  hover:border-teal">
+                <span className="text-lg font-bold ">Topics</span>
+              </div>
+              {allPosts.map((post) => (
+                <div>
+                  <div className="mb-3 leading-tight">
+                    <div>
+                      <button
+                        onClick={() =>
+                          handleTagClick && handleTagClick(post.tag)
+                        }
+                        className="text-teal font-bold"
+                      >
+                        #{post.tag}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mb-3 text-xs">
+        <span className="mr-2">
+          <Link href="/" className="text-grey-darker">
+            © 2023 Chatagraminator
+          </Link>
+        </span>
+        <span className="mr-2">
+          <Link href="/about" className="text-grey-darker">
+            About
+          </Link>
+        </span>
+        <span className="mr-2">
+          <Link href="/help" className="text-grey-darker">
+            Help Center
+          </Link>
+        </span>
+      </div>
+      {/* end of twitter feed */}
     </section>
   );
 };
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,9 +1,15 @@
 "use client";
-
+import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
+import {
+  WhatsappShareButton,
+  WhatsappIcon,
+  LinkedinShareButton,
+  LinkedinIcon
+} from "react-share";
 
 const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { data: session } = useSession();
@@ -28,7 +34,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
 
   return (
     <>
-      <div className="prompt_card">
+      {/* <div className="prompt_card">
         <div className="flex justify-between items-start gap-5">
           <div
             className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
@@ -90,7 +96,132 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
             </p>
           </div>
         )}
+      </div> */}
+
+      {/* Feed card */}
+      <div className="flex border-b border-solid border-grey-light">
+        <div className="w-1/8 text-right pl-3 pt-3">
+          <div>
+            <i className="fa fa-thumb-tack text-teal mr-2" />
+          </div>
+        </div>
+        <div className="w-7/8 p-3 pl-0">
+          <div className="flex justify-between">
+            <div onClick={handleProfileClick}>
+              <div className="flex">
+                <Image
+                  src={post.creator.image}
+                  alt="user_image"
+                  width={40}
+                  height={40}
+                  className="rounded-full object-contain"
+                />
+                <div className="ml-2 cursor-pointer">
+                  <div className="font-bold">
+                    <div onClick={handleProfileClick} className="text-black">
+                      {post.creator.username}
+                    </div>
+                  </div>
+                  <div className="text-grey-dark">{post.creator.email}</div>
+                </div>
+              </div>
+              {/* <span className="text-grey-dark">15 Dec 2017</span> */}
+            </div>
+            <div>
+              
+            </div>
+          </div>
+          <div>
+            <div className="mb-4">
+              <div className="mb-6 flex justify-end">
+                {/* <span>🎉 Tailwind CSS v0.4.0 is out!</span> */}
+                <span className="flex justify-end items-center gap-2">
+                  <span>
+                    <WhatsappShareButton url={post.prompt}>
+                      <WhatsappIcon
+                        size={27}
+                        style={{ borderRadius: "100px", marginTop: "3px" }}
+                      />
+                    </WhatsappShareButton>
+                  </span>
+
+                  <span>
+                    <LinkedinShareButton url={post.prompt}>
+                      <LinkedinIcon
+                        size={27}
+                        style={{ borderRadius: "100px", marginTop: "3px" }}
+                      />
+                    </LinkedinShareButton>
+                  </span>
+
+                  <span
+                    className="copy_btn flex justify-center items-center"
+                    onClick={handleCopy}
+                  >
+                    <Image
+                      src={
+                        copied === post.prompt
+                          ? "/assets/icons/tick.svg"
+                          : "/assets/icons/copy.svg"
+                      }
+                      alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+                      width={12}
+                      height={12}
+                    />
+                  </span>
+                </span>
+              </div>
+
+              <p className="mb-6">{post.prompt}</p>
+              {/* <p className="mb-4">
+                <Link href="#" className="text-teal">
+                  github.com/tailwindcss/ta...
+                </Link>
+              </p> */}
+              <p>
+                <div className="w-full h-72 bg-center">
+                  <img
+                    src={`https://source.unsplash.com/random/900×700/?${post.tag}`}
+                    alt="tweet image"
+                    className="object-cover w-full h-full border border-solid border-grey-light rounded-sm"
+                  />
+                </div>
+              </p>
+            </div>
+          </div>
+          <div className="pb-2">
+            <span className="flex justify-center items-center">
+              {session?.user.id === post.creator._id &&
+                pathName === "/profile" && (
+                  <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+                    <p
+                      className="font-inter text-sm green_gradient cursor-pointer"
+                      onClick={handleEdit}
+                    >
+                      Edit
+                    </p>
+                    <p
+                      className="font-inter text-sm orange_gradient cursor-pointer"
+                      onClick={handleDelete}
+                    >
+                      Delete
+                    </p>
+                  </div>
+                )}
+            </span>
+            <span>
+              <p
+                className="font-inter text-sm blue_gradient cursor-pointer"
+                onClick={() => handleTagClick && handleTagClick(post.tag)}
+              >
+                #{post.tag}
+              </p>
+            </span>
+          </div>
+        </div>
       </div>
+
+      {/* Feed card */}
     </>
   );
 };
